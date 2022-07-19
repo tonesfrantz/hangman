cb1e37fec0e5d897d4c82ad07076ede8be49e34d
anthonyfrantz <tonesfrantz@gmail.com>
2022-07-20 06:54:27 +1000

    hangman_logic

--- README.md
+++ README.md
@@ -2,7 +2,25 @@
 
 traditional hangman game
 
-# plam:
+# plan:
 
--- Generate a random word
---
+    -- Generate a random word
+    -- Store the random word
+    -- Build array, same length as random word filled with Dashes.
+    -- input a guess
+    -- capture the incorrect guess (in guess storage)
+    -- check vs guess amount limit.
+    -- loop over random word and insert at correct index of guessArray.
+
+    -- mark counter of correctGuesses: When === randomArray.length === WIN
+            else continue back to line 10
+    --
+
+Get working usiing console.log()
+
+-- Consider words with multiplle letters that appear more than once.
+
+# additional functionality:
+
+    --change player
+    --UI place holders for each letter.
--- main.js
+++ main.js
@@ -1,1 +1,77 @@
+let randomWord = document.querySelector('.word').textContent;
+let guesses = document.querySelector('.guesses').textContent;
+let randomWordArray = [];
+let input = document.querySelector('.input').value;
 
+const word = [
+    'Got',
+    'ability',
+    'shop',
+    'recall',
+    'fruit',
+    'easy',
+    'dirty',
+    'giant',
+    'shaking',
+    'ground',
+    'weather',
+    'lesson',
+    'almost',
+    'square',
+    'forward',
+    'bend',
+    'cold',
+    'broken',
+    'distant',
+    'adjective',
+    'disolve',
+    'orchestra',
+];
+//Generate random word
+const randomWordGen = function (word) {
+    let rndNum = Math.trunc(Math.random() * word.length) + 1;
+    randomWord = word[rndNum];
+    randomWordArray = randomWord.split('');
+    let rndWordLength = randomWordArray.length - 1;
+    guesses = '';
+    for (let i = 0; i <= rndWordLength; i++) {
+        guesses += ' █ ';
+    }
+    document.querySelector('.guesses').textContent = guesses;
+    document.querySelector('.word').textContent = randomWord;
+    console.log(randomWord);
+    console.log(randomWordArray);
+    return randomWord;
+};
+randomWordGen(word);
+
+//PLAYER INPUT VALUE
+document.querySelector('.submit').addEventListener('click', function (e) {
+    input = document.querySelector('.input').value;
+    let inputLowerCase = input.toLowerCase();
+    checkPlayerInput(inputLowerCase);
+    console.log(inputLowerCase);
+});
+
+//Check player input
+const checkPlayerInput = function (playerInput) {
+    for (let i = 0; i <= randomWordArray.length; i++) {
+        if (playerInput === randomWordArray[i]) {
+            console.log(i);
+            // console.log(addStr(guesses, i, playerInput));
+            document.querySelector('.guesses').textContent = guesses;
+            console.log(guesses);
+            console.log('Winner');
+        } else {
+            console.log('Loser!');
+        }
+    }
+};
+
+function addStr(str, index, stringToAdd) {
+    guesses =
+        str.substring(0, index) +
+        stringToAdd +
+        str.substring(index + 1, str.length - 1);
+    return guesses;
+}
