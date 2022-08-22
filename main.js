b67f481f7d44017d56cf9c16115ed5af4928ef23
anthonyfrantz <tonesfrantz@gmail.com>
2022-08-22 18:27:24 +1000

    logic_update_planning

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const words = ['random', 'word', 'rainbow', 'supermarket', 'celestial'];
+const words = [
+    'zoom',
+    'feel',
+    // 'random',
+    // 'word',
+    // 'rainbow',
+    // 'supermarket',
+    // 'celestial',
+    // 'southern',
+];
 let storedWord = '';
 let storedWordLength = '';
 let correctGuesses = 0;
@@ -9,10 +18,16 @@ let input = null;
 let incorrectGuesses = [];
 
 const playBtn = document.querySelector('.btn_play');
+const guessBtn = document.querySelector('.btn_submit');
 const guessDisplay = document.querySelector('.guesses');
+const guessInput = document.querySelector('.input');
+const incorrect = document.querySelector('.incorrect');
 
 playBtn.addEventListener('click', () => {
     randomWord(words);
+});
+guessBtn.addEventListener('click', () => {
+    playerGuess();
 });
 
 function randomWord(array) {
@@ -31,27 +46,46 @@ function randomWord(array) {
 
 function playerGuess() {
     let guessValue = false;
+
+    if (input === null) {
+        input = guessInput.value;
+        console.log(input);
+    }
+    for (let x = 0; x < storedWord.length; x++) {
+        if (input === storedWord[x]) {
+            console.log(`hello`);
+            guessOutputDash.splice(x, 1, input);
+            console.log(guessOutputDash);
+            guessDisplay.textContent = guessOutputDash.toString();
+            correctGuesses++;
+            console.log(correctGuesses);
+            console.log(`correct ${input}`);
+            guessValue = true;
+        }
+    }
+
+    if (guessValue === false) {
+        incorrectGuesses.push(input);
+        console.log(incorrectGuesses);
+        incorrect.textContent = incorrectGuesses.toString();
+        console.log(`incorrect ${incorrectGuesses}`);
+    }
+    guessValue = false;
     if (correctGuesses === storedWordLength) {
         alert('You WON');
         return;
     }
-    if (input === null) {
-        input = prompt('Please enter a letter');
-    }
-    for (let x = 0; x < storedWord.length; x++) {
-        if (input === storedWord[x]) {
-            guessOutputDash.splice(x, 1, input);
-            correctGuesses++;
-            guessValue = true;
-        }
-    }
-    if (guessValue === false) {
-        incorrectGuesses.push(input);
-    }
-    guessValue = false;
+    input = null;
+    guessInput.value = null;
 }
-playerGuess();
+// playerGuess();
+
+// Write some tests.
+// - Testing suite for Javascript. (mocha or chai?)
 
 // Have managed to display dashed correctly.
+
 // Next DOM make the inpute work and add the letter to the dashes if correct or display incorrect guessed.
 // Create counter for incorrect guesses.
+// Refactor all if and for.
+// Rename variables & functions.
